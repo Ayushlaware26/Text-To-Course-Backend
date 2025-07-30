8a4bda9e32dbed5201218bf3ca402c5efc5021be
Ayush Laware <ayushlaware@gmail.com>
2025-07-30 17:19:10 +0530

    generate_course view added

--- course/views.py
+++ course/views.py
@@ -1,7 +1,13 @@
 from django.http import JsonResponse
+from .auth_utils import require_auth
 
+@require_auth
 def generate_course(request):
+    # You can access token payload here
+    user_info = request.auth_payload
+
+    # For now, just return dummy data
     return JsonResponse({
-        "message": "Backend is working!",
-        "sample": "This will later generate content"
+        "message": "This is a protected endpoint!",
+        "user": user_info
     })
